86fbcfa8632b4216926ea09b8121f95513e4d0bc
Higher Stark <higherwashington@gmail.com>
2018-03-08 10:52:48 +0800

    unit test added

--- src/WordLadderTest.java
+++ src/WordLadderTest.java
@@ -5,12 +5,24 @@ import components.WordLadder;
 
 public class WordLadderTest{
 
-		@Test
-		public void testFindLadder() {
-				HashSet<String> dict = new HashSet<String>();
-				dict.add("cat");
-				WordLadder wl = new WordLadder("cat", "cat", dict);
-				String res = wl.findLadder();
-				assertEquals(res, "cat");
+	@Test
+	public void testFindLadder() {
+		HashSet<String> dict = new HashSet<String>();
+		dict.add("cat");
+		WordLadder wl = new WordLadder("cat", "cat", dict);
+		String res = wl.findLadder();
+		assertEquals(res, "cat");
+	}
+
+	@Test
+	public void testFindLadder2() {
+		HashSet<String> dict = new HashSet<String>();
+		String[] list = {"cat", "dog", "java", "bat", "rat", "bot", "dot", "test", "english"};
+		for (String s : list) {
+			dict.add(s);
 		}
+		WordLadder wl = new WordLadder("cat", "dog", dict);
+		String res = wl.findLadder();
+		assertEquals("cat -> bat -> bot -> dot -> dog", res);
+	}
 }
